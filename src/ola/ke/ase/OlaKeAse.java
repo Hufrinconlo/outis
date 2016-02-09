a6a65f636845a5dec15abc77f4f9be68d3a154fa
Usuario21 <Usuario21@FI4531119-21>
2016-02-09 15:36:20 -0500

    segui

--- src/ola/ke/ase/OlaKeAse.java
+++ src/ola/ke/ase/OlaKeAse.java
@@ -3,6 +3,6 @@ package ola.ke.ase;
 public class OlaKeAse {
 
       public static void main(String[] args) {
-    System.out.println("ola ke ase");
+    System.out.println("ola ke ase, estudiando o ke ase");
   }
     }
